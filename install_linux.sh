b5d330240f51d32bdd8df920b3580227a2cc817f
Richard Julian <richard@rjulian.net>
2019-12-28 10:03:30 -0800

    Expand install script a bit more.

--- install_linux.sh
+++ install_linux.sh
@@ -1,3 +1,25 @@
-sudo apt-get update && sudo apt-get -y install aircrack-ng
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+    echo "Please provide network interface name as argument."
+fi
+
+echo "Checking if airodump binary is installed."
+echo "========================================="
+if [[ -x "/usr/sbin/airodump-ng" ]]
+then
+    echo "Airodump-ng has been found."
+else
+    sudo apt-get update && sudo apt-get -y install aircrack-ng
+fi
+
+echo "========================================="
+echo "Starting interface $1 in monitor mode."
+echo "========================================="
 sudo airmon-ng $1
+
+echo "========================================="
+echo "Starting airodump on $1mon."
+echo "========================================="
 sudo airodump-ng --beacons -o csv -w whoiswalkingby $1mon
